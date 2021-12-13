009f8f8564a5c92a6cb36a8cb0186bf85bdc16b2
jtsmedley <38006759+jtsmedley@users.noreply.github.com>
2021-12-12 22:29:37 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,8 @@ else
     echo "$FILE does not exist."
 fi
 echo "Hostname: $ESTUARY_HOSTNAME"
-/usr/src/estuary/estuary-shuttle --dev --estuary-api $ESTUARY_HOSTNAME --auth-token $ESTUARY_SHUTTLE_TOKEN --handle $ESTUARY_SHUTTLE_HANDLE
+if [ -v DEVENV ]; then
+    /usr/src/estuary/estuary-shuttle --dev --estuary-api $ESTUARY_HOSTNAME --auth-token $ESTUARY_SHUTTLE_TOKEN --handle $ESTUARY_SHUTTLE_HANDLE
+else
+    /usr/src/estuary/estuary-shuttle --estuary-api $ESTUARY_HOSTNAME --auth-token $ESTUARY_SHUTTLE_TOKEN --handle $ESTUARY_SHUTTLE_HANDLE
+fi
